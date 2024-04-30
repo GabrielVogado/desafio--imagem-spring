8f748b16f9a1b03af4feeabfe83b76d21fb15c3d
Gabriel Vogado <gabriel.vogado@gmail.com>
2024-04-30 19:41:17 -0300

    initial commit

---
+++ src/main/java/com/desafio/spring/desafioimagemspring/DesafioImagemSpringApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.spring.desafioimagemspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioImagemSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioImagemSpringApplication.class, args);
+    }
+
+}
